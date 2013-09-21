53e767860c59b4a62f457b3c225ad69ff64b642d
Zhuo <zhuoc@cs.cmu.edu>
2013-09-21 16:13:01 -0400

    initial web interface

--- server/quiltview/quiltview/urls.py
+++ server/quiltview/quiltview/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'quiltview.views.home', name='home'),
     # url(r'^quiltview/', include('quiltview.foo.urls')),
+    url(r'^quiltview/$', 'quiltview_query.views.index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- server/quiltview/quiltview_query/views.py
+++ server/quiltview/quiltview_query/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from quiltview_query.models import User, Query, Video, Prompt
+
+# rendering
+from django.template import loader, RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+
+import datetime
+from django.utils import timezone
+
+
+def index(request):
+    c = RequestContext(request, {
+    })
+    return render_to_response('quiltview/index.html', {}, RequestContext(request))
+
