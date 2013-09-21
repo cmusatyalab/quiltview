f5bc0289be2b462042b015caf041eaf352e609b6
Zhuo <zhuoc@cs.cmu.edu>
2013-09-21 13:57:24 -0400

    update database model

--- server/quiltview/quiltview_query/models.py
+++ server/quiltview/quiltview_query/models.py
@@ -1,40 +1,16 @@
 from django.db import models
 from django.utils import timezone
-import my_configure
-
-class Query(models.Model):
-    # time and location
-    requested_time = models.DateTimeField(default = timezone.now())
-    latest_upload_time = models.DateTimeField()
-    interest_location_lat = models.DecimalField(max_digits = 12, decimal_places = 8)
-    interest_location_long = models.DecimalField(max_digits = 12, decimal_places = 8)
-
-    # users
-    requester = models.ForeignKey(User)
-    users_asked = models.ManyToManyField(User)
-    users_responded = models.ManyToManyField(User)
-
-    # query content
-    content = models.CharField(max_length = 140)
-    time_out = models.IntegerField()
-    back_time_allowance = models.IntegerField()
-    reward = models.IntegerField()
-
-    # responses
-    cache_hit = models.BooleanField()
-    reload_query = models.BooleanField()
-    video_responses = models.ManyToManyField(Video)
-
-    def __unicode__(self):
-        return self.content
-
-    class Meta:
-        db_table = "quiltview_query"
 
 class User(models.Model):
     # status
     is_active = models.BooleanField()
-    context = models.IntegerField(default = my_configure.CONTEXT_IDLE)
+    CONTEXT_CHOICES = (
+        ('ID', 'Idle'),
+        ('BS', 'Busy'),
+        ('DR', 'Driving'),
+        ('SL', 'Sleeping'),
+    )
+    context = models.CharField(max_length = 2, choices = CONTEXT_CHOICES)
     location_lat = models.DecimalField(max_digits = 12, decimal_places = 8)
     location_long = models.DecimalField(max_digits = 12, decimal_places = 8)
 
@@ -45,7 +21,13 @@ class User(models.Model):
 
     # preferences
     max_upload_time = models.IntegerField()
-    max_upload_unit = models.IntegerField()   #ENUM
+    UNIT_CHOICES = (
+        ('MM', 'Month'),
+        ('DD', 'Day'),
+        ('HH', 'Hour'),
+        ('MI', 'Minute'),
+    )
+    max_upload_unit = models.CharField(max_length = 2, choices = UNIT_CHOICES)
     other_preferences = models.CharField(max_length = 255)  # has a risk of exceeding MySQL's max_length of VARCHAR field
 
     def __unicode__(self):
@@ -53,6 +35,35 @@ class User(models.Model):
 
     class Meta:
         db_table = "quiltview_user"
+
+class Query(models.Model):
+    # time and location
+    requested_time = models.DateTimeField(default = timezone.now())
+    latest_upload_time = models.DateTimeField()
+    interest_location_lat = models.DecimalField(max_digits = 12, decimal_places = 8)
+    interest_location_long = models.DecimalField(max_digits = 12, decimal_places = 8)
+
+    # users
+    requester = models.ForeignKey(User)
+    #users_asked = models.ManyToManyField(User)
+    #users_responded = models.ManyToManyField(User)
+
+    # query content
+    content = models.CharField(max_length = 140)
+    time_out = models.IntegerField()
+    back_time_allowance = models.IntegerField()
+    reward = models.IntegerField()
+
+    # responses
+    cache_hit = models.BooleanField()
+    reload_query = models.BooleanField()
+    #video_responses = models.ManyToManyField(Video)
+
+    def __unicode__(self):
+        return self.content
+
+    class Meta:
+        db_table = "quiltview_query"
 
 class Video(models.Model):
     owner = models.ForeignKey(User)
@@ -77,7 +88,11 @@ class Prompt(models.Model):
     responded_time = models.DateTimeField()
     user_location_lat = models.DecimalField(max_digits = 12, decimal_places = 8)
     user_location_long = models.DecimalField(max_digits = 12, decimal_places = 8)
-    action = models.IntegerField()  #ENUM
+    ACTION_CHOICES = (
+        ('Y', 'Replied'),
+        ('N', 'Rejected'),
+    )
+    action = models.CharField(max_length = 2, choices = ACTION_CHOICES)
  
     def __unicode__(self):
         return self.id
