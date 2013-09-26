2a742dced32a9b334320ffd54c5abf626c07fa4b
Zhuo <zhuoc@cs.cmu.edu>
2013-09-26 13:05:31 -0400

    proxy server can receive video properly now

--- proxy_server/proxy_server.py
+++ proxy_server/proxy_server.py
@@ -1,22 +1,40 @@
 # TODO: description
 
 import socket
+import struct
 
+TYPE_JSON = 0
+TYPE_LENGTH = 1
+TYPE_VIDEO = 2
 
 def startServer(host, port, buf):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(0)
     while True:
+        print "waiting for connection"
         conn, addr = s.accept()
         print 'Connected by', addr
+        current_type = TYPE_LENGTH
         while True:
             data = conn.recv(buf)
             if not data: 
                 print "Connection terminated by the other side"
                 break
-            print "Got something"
+            if current_type == TYPE_LENGTH:
+                video_len = struct.unpack("!I", data)[0]  # Zhuo: why use this?
+                print "Length = %d" % video_len
+                video_file = open("uploaded_video.mp4", 'w')
+                current_type += 1
+            elif current_type == TYPE_VIDEO:
+                video_file.write(data)
+                
         conn.close()
+        video_file.close()
+
+        # TODO: upload to Youtube, get url back
+
+        # TODO: register new video at QuiltView
 
 if __name__ == "__main__":
     HOST = ""
