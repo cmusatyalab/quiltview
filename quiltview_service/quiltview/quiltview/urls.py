c3d19b0e813aa2888d8024e52cacaf06b989b11d
Zhuo Chen <zhuoc@cs.cmu.edu>
2014-04-24 15:17:50 +0000

    Import needed libraries

--- quiltview_service/quiltview/quiltview/urls.py
+++ quiltview_service/quiltview/quiltview/urls.py
@@ -21,6 +21,8 @@
 from django.conf.urls import patterns, include, url
 from quiltview_query.api import UserResource, QueryResource, VideoResource, PromptResource
 from tastypie.api import Api
+from django.conf import settings
+from django.conf.urls.static import static
 #from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
