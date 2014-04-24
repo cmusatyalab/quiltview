070eee8a1042c61b8b42a52197772f1d2ac35f54
Zhuo Chen <zhuoc@cs.cmu.edu>
2014-04-24 10:21:49 -0400

    Added media serve for quiltview service

--- quiltview_service/quiltview/quiltview/urls.py
+++ quiltview_service/quiltview/quiltview/urls.py
@@ -55,4 +55,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^api/', include(dm.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
